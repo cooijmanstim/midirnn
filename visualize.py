0f9bbca8ef0ce5380e21f14f339e99fc937f560e
Anna Huang <chengzhiannahuang@gmail.com>
2015-10-27 20:12:41 -0400

    add visualize module to plot the most recent generated output

---
+++ visualize.py
@@ -0,0 +1,40 @@
+import os
+import glob
+import logging
+import numpy as np
+import pylab as plt
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def plot_matrix(xs):
+    plt.matshow(xs[:, 0, :].T, cmap='gray')
+    plt.show()
+
+
+def load_from_npz(fname):
+    samples = np.load(fname)
+    xs = samples["xs"]
+    return xs
+
+
+def retrieve_most_recent_fname():
+    fname = max(glob.iglob('*.npz'), key=os.path.getctime)
+    logger.info("The most recent file is %s." % fname)
+    return fname
+
+
+def plot_sample(fname):
+    xs = load_from_npz(fname)
+    plot_matrix(xs)
+
+
+def plot_most_recent_sample():
+    fname = retrieve_most_recent_fname()
+    xs = load_from_npz(fname)
+    plot_matrix(xs)
+
+
+if __name__ == "__main__":
+    plot_most_recent_sample()
